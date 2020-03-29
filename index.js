aa6c47d2411cbdd5bb8d0fdd8841512ea2b05d07
anupama <anupamagadde@yahoo.com>
2020-03-29 01:53:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let paragraphElementToChange = document.getElementById("text")
+    paragraphElementToChange.textContent = "This is really cool!"
+  });
+   
